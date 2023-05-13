a6ec4ec368a5404370c626a0cde1a55ce66773fe
Marisol Gironzi <gironzimarisol@gmail.com>
2023-05-13 11:50:33 -0300

    routing login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
-  {path: '', pathMatch: 'full'},
+  {path: '', pathMatch: 'full', redirectTo:'login'},
   {path: 'login', component: LoginComponent},
 
   {path: '**', redirectTo: 'login', pathMatch:'full'},
